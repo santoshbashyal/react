b85567c45dc931032efe419b9e24c53c71c99dae
Santosh Bashyal <santoshbashyal000@gmail.com>
2025-05-16 13:14:59 +0545

    form-validation

--- src/pages/auth/loginpage/loginpage.jsx
+++ src/pages/auth/loginpage/loginpage.jsx
@@ -1,30 +1,21 @@
-
-
-
-
-
 // Life cycle of component : 3 Stages
 
-// Stage1: 
-// a. Mounting Phase : 
-        // constructor()=> render() => componentDidMount => (State update:)=> render()
-// b. Updating Phase: 
-    // 
-// c. Unmounting Phase: 
-
-
-
-
-
+// Stage1:
+// a. Mounting Phase :
+// constructor()=> render() => componentDidMount => (State update:)=> render()
+// b. Updating Phase:
+//
+// c. Unmounting Phase:
 
 // import './../../../assets/css/main.css'
 import React from "react";
 import "flowbite";
+import { NavLink } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
 
 // import { DarkThemeToggle } from 'flowbite-react';
 
-
-// className LoginPage extends React.Component{
+// classNameName LoginPage extends React.Component{
 
 //     constructor(){
 //         super() //parent ko constructor lai call garchha
@@ -39,7 +30,7 @@ import "flowbite";
 
 //     componentDidMount=()=>{
 //         console.log("Im in componentDidMount")
-//         // networkCall, Api call 
+//         // networkCall, Api call
 //         // stateUpdate
 //         this.setState((prev)=>{
 //             console.log(prev)
@@ -73,13 +64,143 @@ import "flowbite";
 //      )
 //     }
 // }
-const LoginPage = ()=>{
-    // data store state
-    // state create by using hook 
-    // effect hook
+const LoginPage = () => {
+  // data store state
+  // state create by using hook
+  // effect hook
 
-    return(
-      <h1>s</h1>
-    ) 
-}
-export default LoginPage
+  return (
+    <>
+      {/* <section classNameName="bg-gray-50 py-8 antialiased dark:bg-primary-900 md:py-16">
+        <div classNameName="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          <div classNameName="mb-4 items-center justify-between gap-4 md:mb-8">
+            <h2 classNameName="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+              Sign In
+            </h2>
+            <hr classNameName="w-full bg-gray-50 dark:bg-gray-100"></hr>
+          </div>
+
+          <div classNameName="mb-4 text-gray-800 dark:text-gray-100 ">
+            <form action="">
+              <div classNameName="flex mb-5">
+                <label htmlhtmlFor="username" classNameName="w-1/3">
+                  Username:{" "}
+                </label>
+                <input
+                  classNameName="text-gray-800 w-2/3 rounded-2xl"
+                  type="email"
+                  name="email"
+                  id="username"
+                  placeholder="Enter your email...."
+                  required={true}
+                />
+              </div>
+              <div classNameName="flex">
+                <label htmlhtmlFor="password" classNameName="w-1/3">
+                  Password:{" "}
+                </label>
+                <input
+                  classNameName="text-gray-800 w-2/3 rounded-2xl"
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password...."
+                  required={true}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section> */}
+
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        htmlFor="remember"
+                        className="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign up
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default LoginPage;
